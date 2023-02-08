9962b0ece671308e95519a1c57f040eefe0e8282
Izayer <iker_izagirre@hotmail.com>
2023-02-08 15:49:14 +0100

    header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,10 +19,12 @@ import { MdbTabsModule } from 'mdb-angular-ui-kit/tabs';
 import { MdbTooltipModule } from 'mdb-angular-ui-kit/tooltip';
 import { MdbValidationModule } from 'mdb-angular-ui-kit/validation';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
