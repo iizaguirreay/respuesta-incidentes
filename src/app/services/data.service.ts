ea46f84b686ab47234b8befccc2d514392e75a1c
iizaguirreay <iizaguirre@ziur.eus>
2023-02-10 10:07:42 +0100

    ZIUR logo eta ordenatu

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -14,38 +14,38 @@ export class DataService {
   private grupos = [
     {
       id: 1,
-      title: "Recursos Humanos",
-      description: "Garantizar el bienestar laboral de los trabajadores para maximizar su productividad en la organización y evitar ataques internos de empleados",
-      icon: "far fa-address-book",
-      backgroundcolor: "bg-recursos-humanos"
+      title: "Comunicación",
+      description: "",
+      icon: "fas fa-headset",
+      backgroundcolor: "bg-comunicacion"
     },
     {
       id: 2,
-      title: "Formación y Concienciación",
+      title: "Respuesta ante Incidentes",
       description: "",
-      icon: "fas fa-graduation-cap",
-      backgroundcolor: "bg-formacion"
+      icon: "fas fa-user-md",
+      backgroundcolor: "bg-respuesta-incidentes"
     },
     {
       id: 3,
-      title: "Control de Acceso Físico",
-      description: "Mecanismo de seguridad que verifica la identidad y los permisos del individuo que desea acceder a un activo físico",
-      icon:"fas fa-door-closed",
-      backgroundcolor: "bg-acceso-fisico"
-    },
-    {
-      id: 4,
       title: "Threath Hunting - Inteligencia de Amenazas",
       description: "Capacidad de recolección, análisis e intercambio de información acerca de las ciberamenazas existentes",
       icon: "fas fa-brain",
       backgroundcolor: "bg-threat-hunting"
     },
     {
-      id: 5,
+      id: 4,
       title: "Monitorización",
       description: "",
       icon: "fas fa-desktop",
       backgroundcolor: "bg-monitorizacion"
+    },
+    {
+      id: 5,
+      title: "Gestión de Identidades",
+      description: "",
+      icon: "far fa-id-card",
+      backgroundcolor: "bg-gestion-identidad"
     },
     {
       id: 6,
@@ -56,38 +56,38 @@ export class DataService {
     },
     {
       id: 7,
-      title: "Gestión de Identidades",
-      description: "",
-      icon: "far fa-id-card",
-      backgroundcolor: "bg-gestion-identidad"
+      title: "Prevención de Amenazas",
+      description: "Protección, prevención y bloqueo de amenazas avanzadas que pretendan comprometer los activos de la organización",
+      icon: "fas fa-shield-alt",
+      backgroundcolor: "bg-proteccion-amenazas"
     },
     {
       id: 8,
-      title: "Respuesta ante Incidentes",
-      description: "",
-      icon: "fas fa-user-md",
-      backgroundcolor: "bg-respuesta-incidentes"
-    },
-    {
-      id: 9,
-      title: "Comunicación",
-      description: "",
-      icon: "fas fa-headset",
-      backgroundcolor: "bg-comunicacion"
-    },
-    {
-      id: 10,
       title: "Plan de Continuidad",
       description: "Plan logístico que permite continuar la actividad de la empresa ante un ciberincidente que pueda provocar una interrupción en las operaciones críticas del negocio",
       icon: "fas fa-calendar-check",
       backgroundcolor: "bg-plan-continuidad"
     },
     {
+      id: 9,
+      title: "Formación y Concienciación",
+      description: "",
+      icon: "fas fa-graduation-cap",
+      backgroundcolor: "bg-formacion"
+    },
+    {
+      id: 10,
+      title: "Control de Acceso Físico",
+      description: "Mecanismo de seguridad que verifica la identidad y los permisos del individuo que desea acceder a un activo físico",
+      icon:"fas fa-door-closed",
+      backgroundcolor: "bg-acceso-fisico"
+    },
+    {
       id: 11,
-      title: "Prevención de Amenazas",
-      description: "Protección, prevención y bloqueo de amenazas avanzadas que pretendan comprometer los activos de la organización",
-      icon: "fas fa-shield-alt",
-      backgroundcolor: "bg-proteccion-amenazas"
+      title: "Recursos Humanos",
+      description: "Garantizar el bienestar laboral de los trabajadores para maximizar su productividad en la organización y evitar ataques internos de empleados",
+      icon: "far fa-address-book",
+      backgroundcolor: "bg-recursos-humanos"
     }
   ];
 
@@ -115,23 +115,23 @@ export class DataService {
         break;  
       }
       case 1: { 
-        this.actions = this.humanos; 
+        this.actions = this.comunicacion; 
         break; 
       } 
       case 2: { 
-        this.actions = this.formacion;
+        this.actions = this.respuesta;
         break; 
       }
       case 3: { 
-        this.actions = this.fisico;
+        this.actions = this.inteligencia;
         break; 
       }
       case 4: { 
-        this.actions = this.inteligencia;
+        this.actions = this.monitorizacion;
         break;  
       }
       case 5: { 
-        this.actions = this.monitorizacion;
+        this.actions = this.identidad;
         break;  
       }
       case 6: { 
@@ -139,23 +139,23 @@ export class DataService {
         break;  
       }
       case 7: { 
-        this.actions = this.identidad;
+        this.actions = this.prevencion;
         break;  
       }
       case 8: { 
-        this.actions = this.respuesta;
-        break;  
-      }
-      case 9: { 
-        this.actions = this.comunicacion;
-        break;  
-      }
-      case 10: { 
         this.actions = this.continuidad;
         break;  
       }
+      case 9: { 
+        this.actions = this.formacion;
+        break;  
+      }
+      case 10: { 
+        this.actions = this.fisico;
+        break;  
+      }
       case 11: { 
-        this.actions = this.prevencion;
+        this.actions = this.humanos;
         break;  
       }
       default: { 
@@ -167,86 +167,86 @@ export class DataService {
 
   comunicacion = [
     {
-      id: "CAAC0000",
+      id: "CAAC0001",
       description: "Comunicación Interna"
     },
     {
-      id: "CAAC0001",
+      id: "CAAC0002",
       description: "Comunicación al CERT"
     },
     {
-      id: "CAAC0002",
+      id: "CAAC0003",
       description: "Comunicación a proveedores/clientes"
     },
     {
-      id: "CAAC0003",
+      id: "CAAC0004",
       description: "Comunicación a medios de difusión"
     },
     {
-      id: "CAAC0004",
+      id: "CAAC0005",
       description: "Comunicación a reguladores"
     },
     {
-      id: "CAAC0005",
+      id: "CAAC0006",
       description: "Comunicación a FFCCSE"
     },
     {
-      id: "CAAC0006",
+      id: "CAAC0007",
       description: "Definición estrategia de comunicación"
     },
     {
-      id: "CAAC0013",
+      id: "CAAC0008",
       description: "Nota de prensa"
     },
     {
-      id: "CAAC0014",
+      id: "CAAC0009",
       description: "Campaña en redes sociales"
     },
     {
-      id: "CAAC0052",
+      id: "CAAC0010",
       description: "Comunicación a la AEPD"
     }
   ];
 
   respuesta = [
     {
-      id: "CAAC0007",
+      id: "CAAC0011",
       description: "Aislar máquinas"
     },
     {
-      id: "CAAC0008",
+      id: "CAAC0012",
       description: "Análisis Forense"
     },
     {
-      id: "CAAC0018",
+      id: "CAAC0013",
       description: "Actualizar firmas malware"
     },
     {
-      id: "CAAC0019",
+      id: "CAAC0014",
       description: "Crear línea temporal"
     },
     {
-      id: "CAAC0021",
+      id: "CAAC0015",
       description: "Activar Comité de Crisis"
     },
     {
-      id: "CAAC0022",
+      id: "CAAC0016",
       description: "Análisis Forense del Jump Server"
     },
     {
-      id: "CAAC0031",
+      id: "CAAC0017",
       description: "Crear informe de seguimiento"
     },
     {
-      id: "CAAC0032",
+      id: "CAAC0018",
       description: "Bloqueo de tráfico (RDP)"
     },
     {
-      id: "CAAC0033",
+      id: "CAAC0019",
       description: "Instalar parches de seguridad"
     },
     {
-      id: "CAAC0077",
+      id: "CAAC0020",
       description: "Revisión física del equipo"
     }
 
@@ -254,127 +254,127 @@ export class DataService {
 
   inteligencia = [
     {
-      id: "CAAC0009",
+      id: "CAAC0021",
       description: "Extracción de IOCs"
     },
     {
-      id: "CAAC0010",
+      id: "CAAC0022",
       description: "Enriquecimiento de IOCs"
     },
     {
-      id: "CAAC0012",
+      id: "CAAC0023",
       description: "Busqueda de IOCs"
     },
     {
-      id: "CAAC0020",
+      id: "CAAC0024",
       description: "Intercambio de información"
     }
   ];
 
   monitorizacion = [
     {
-      id: "CAAC0011",
+      id: "CAAC0025",
       description: "Monitorización Activa de Foro"
     },
     {
-      id: "CAAC0049",
+      id: "CAAC0026",
       description: "Revisión del registro de eventos (LOGS)"
     },
     {
-      id: "CAAC0054",
+      id: "CAAC0027",
       description: "Revisar eventos de tráfico de red"
     },
     {
-      id: "CAAC0056",
+      id: "CAAC0028",
       description: "Revisar alertas de DLP"
     }
   ];
 
   identidad = [
     {
-      id: "CAAC0015",
+      id: "CAAC0029",
       description: "Comprobar inventario de usuarios"
     }
   ];
 
   activos = [
     {
-      id: "CAAC0016",
+      id: "CAAC0030",
       description: "Consultar inventario de activos"
     },
     {
-      id: "CAAC0037",
+      id: "CAAC0031",
       description: "Establecer doble factor de autenticación"
     }
   ];
 
   prevencion = [
     {
-      id: "CAAC0017",
+      id: "CAAC0032",
       description: "Reglas detección y alertas"
     },
     {
-      id: "CAAC0030",
+      id: "CAAC0033",
       description: "Evaluar escenario de riesgo"
     },
     {
-      id: "CAAC0041",
+      id: "CAAC0034",
       description: "Reglas de bloqueo de email"
     }
   ];
 
   continuidad = [
     {
-      id: "CAAC0023",
+      id: "CAAC0035",
       description: "Restaurar activos (Mundo IT Main Site)"
     },
     {
-      id: "CAAC0042",
+      id: "CAAC0036",
       description: "Landing page alternativa"
     },
     {
-      id: "CAAC0053",
+      id: "CAAC0037",
       description: "Recuperación de las máquinas afectadas"
     },
     {
-      id: "CAAC0085",
+      id: "CAAC0038",
       description: "Restitución segura del activo"
     }
   ];
 
   formacion = [
     {
-      id: "CAAC0034",
+      id: "CAAC0039",
       description: "Concienciación Ingeniería Social"
     },
     {
-      id: "CAAC0055",
+      id: "CAAC0040",
       description: "Recomendaciones Buenas prácticas a los usuarios"
     }
   ];
 
   fisico = [
     {
-      id: "CAAC0044",
+      id: "CAAC0041",
       description: "Revisión accesos físicos"
     },
     {
-      id: "CAAC0045",
+      id: "CAAC0042",
       description: "Revisión cámaras de vigilancia"
     },
     {
-      id: "CAAC0046",
+      id: "CAAC0043",
       description: "Bloqueo de accesos físicos"
     }
   ];
 
   humanos = [
     {
-      id: "CAAC0047",
+      id: "CAAC0044",
       description: "Revisión del procedimiento de bajas"
     },
     {
-      id: "CAAC0048",
+      id: "CAAC0045",
       description: "Entrevista con ex-empleado"
     }
   ];
